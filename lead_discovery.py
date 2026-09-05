60c40f01869e58a5b554a7ca6d23ff0619af87f4
Joseph Onyifx <josephstones100@gmail.com>
2026-09-05 07:33:17 +0100

    Refactor: migrate from deprecated duckduckgo_search to ddgs package

--- lead_discovery.py
+++ lead_discovery.py
@@ -4,7 +4,7 @@ import time
 import urllib.parse
 import requests
 from bs4 import BeautifulSoup
-from duckduckgo_search import DDGS
+from ddgs import DDGS
 from db import supabase
 
 # Global Target Parameters
